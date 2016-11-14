dd7a022438a0d9032816737ef96e57f76a7b40b3
Ambre Thomas <embrynette@gmail.com>
2016-11-14 10:21:24 +0100

    modif ft_strrchr.c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -10,15 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_strlen(char const *s)
-{
-	int i;
-
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
+#include "libft.h"
 
 char	*ft_strrchr(char const *s, int c)
 {
